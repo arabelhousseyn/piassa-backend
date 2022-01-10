c70d5dfb6db3e3a28904504d2f73af59607f1158
arabelhousseyn <potency.football@gmail.com>
2022-01-10 13:44:17 +0000

    create SellerJob

---
+++ database/migrations/2022_01_10_132941_create_seller_jobs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSellerJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seller_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('job');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seller_jobs');
+    }
+}
