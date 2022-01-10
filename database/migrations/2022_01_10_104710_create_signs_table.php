ed00a2efdd6f0d1dccba48899b5f671e443f8be1
arabelhousseyn <potency.football@gmail.com>
2022-01-10 10:50:29 +0000

    add sign table

---
+++ database/migrations/2022_01_10_104710_create_signs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('signs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('logo');
+            $table->string('percent');
+            $table->string('prefix');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('signs');
+    }
+}
