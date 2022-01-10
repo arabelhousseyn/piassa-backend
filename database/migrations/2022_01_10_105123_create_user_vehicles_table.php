ca1b140d1c5160293f1e02b12f057084a8b91fa2
arabelhousseyn <potency.football@gmail.com>
2022-01-10 10:57:37 +0000

    create user vehicle model

---
+++ database/migrations/2022_01_10_105123_create_user_vehicles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('sign_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('model');
+            $table->year('year');
+            $table->string('motorization');
+            $table->string('chassis_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_vehicles');
+    }
+}
