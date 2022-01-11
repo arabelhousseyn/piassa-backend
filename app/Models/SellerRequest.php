43f542cbe52ac66c62cea1d621c3d5118a1cebaf
arabelhousseyn <potency.football@gmail.com>
2022-01-11 08:24:10 +0000

    create SellerRequest model

---
+++ app/Models/SellerRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SellerRequest extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'seller_id',
+        'request_id',
+        'suggest_him_at'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+}
---
+++ database/migrations/2022_01_11_082103_create_seller_requests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSellerRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seller_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('request_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamp('suggest_him_at');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seller_requests');
+    }
+}
