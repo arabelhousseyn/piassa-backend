b01a88820102464356a866b2607a52bc19304657
arabelhousseyn <potency.football@gmail.com>
2022-01-20 13:18:09 +0000

    create routes for cart

--- routes/api.php
+++ routes/api.php
@@ -45,10 +45,19 @@ Route::prefix('seller')->group(function(){
 Route::middleware('auth:sanctum')->group(function(){
 
     Route::prefix('/user')->group(function(){
+
         Route::get('insert_location/{location?}',[UserController::class,'insert_location']);
         Route::get('list_suggestions_request/{request_id}',[UserController::class,'list_suggestions_request'])->whereNumber('request_id');
         Route::get('user_list_requests_by_vehicle/{user_vehicle_id}',[UserController::class,'user_list_requests_by_vehicle'])->whereNumber('user_vehicle_id');
         Route::get('count_suggestions_request/{request_id}',[UserController::class,'count_suggestions_request'])->whereNumber('request_id');
+
+        // cart
+        Route::prefix('cart')->group(function(){
+            Route::post('store',UserController::class,'store_cart');
+            Route::delete('destory_item',UserController::class,'destory_items_cart');
+            Route::delete('destory',UserController::class,'destory_cart');
+        });
+
     });
 
     Route::prefix('/seller')->group(function(){
