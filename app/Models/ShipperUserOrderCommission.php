c98db5b6a11e13327c41a32505a199467fd3a28a
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-01-25 13:10:45 +0000

    create ShipperUserOrderCommission model

---
+++ app/Models/ShipperUserOrderCommission.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ShipperUserOrderCommission extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'shipper_user_order_id',
+        'start_coordination',
+        'end_coordination',
+        'amount'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'deleted_at'
+    ];
+}
---
+++ database/migrations/2022_01_25_130815_create_shipper_user_order_commissions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShipperUserOrderCommissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipper_user_order_commissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('shipper_user_order_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('start_coordination');
+            $table->string('end_coordination')->nullable();
+            $table->double('amount')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipper_user_order_commissions');
+    }
+}
