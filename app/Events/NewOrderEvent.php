38f926bbbee611d48f733ed25ca52a71ffb40b93
arabelhousseyn <potency.football@gmail.com>
2022-01-25 10:59:30 +0000

    add implements for interface ShouldBroadcast in NewOrderEvent.php

--- app/Events/NewOrderEvent.php
+++ app/Events/NewOrderEvent.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NewOrderEvent
+class NewOrderEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $data;
