2de382140ce5c74655f3b3232951a512bf48bfe9
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-02-02 16:21:09 +0000

    changes

--- app/Http/Controllers/Api/V1/LoginController.php
+++ app/Http/Controllers/Api/V1/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 class LoginController extends Controller
