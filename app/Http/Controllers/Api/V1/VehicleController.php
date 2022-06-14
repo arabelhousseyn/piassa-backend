56a3270b4086fdfa5ac4d86ce951717cb5a034dd
arabelhousseyn <hocine.arab1@hotmail.com>
2022-06-14 12:03:43 +0100

    make some changes

--- app/Http/Controllers/Api/V1/VehicleController.php
+++ app/Http/Controllers/Api/V1/VehicleController.php
@@ -100,7 +100,8 @@ class VehicleController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        UserVehicle::where('user_id',$id)->update([$request->all()]);
+        return response()->noContent();
     }
 
     /**
