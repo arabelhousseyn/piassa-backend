7155e10022bbf70de7eaf9dcf1043dc63a375a15
arabelhousseyn <hocine.arab1@hotmail.com>
2022-07-04 11:09:14 +0100

    make some changes

--- app/Services/RequestUserService.php
+++ app/Services/RequestUserService.php
@@ -94,54 +94,21 @@ class RequestUserService{
                 if($open)
                 {
                     $seller_ids[] = $seller->id;
-                    $info1 = explode(',',$seller->profile->location);
-                    $info2 = explode(',',$user->locations[0]->location);
-                    $distance = GeoFacade::setPoint([doubleval($info1[0]), doubleval($info1[1])])
-                        ->setOptions(['units' => ['km']])
-                        ->setPoint([doubleval($info2[0]), doubleval($info2[1])])
-                        ->getDistance();
-
-                    if(array_key_exists('2-3',$distance))
-                    {
-                        $arr = [
-                            'seller_id' => $seller->id,
-                            'distance' => $distance['2-3']['km']
-                        ];
-                        $distances[] = $arr;
-                    }elseif(array_key_exists('1-2',$distance)){
-
-                        $arr = [
-                            'seller_id' => $seller->id,
-                            'distance' => $distance['1-2']['km']
-                        ];
-                        $distances[] = $arr;
-                    }
                     $data = UserRequest::with('informations')->find($operation->id);
                     event(New NewRequestForSellerEvent($data,$seller->id));
                     $sellers = Seller::whereIn('id',[$seller->id])->get();
+
+                    $seller = Seller::find($seller->id);
+                    $seller->requests()->create([
+                        'user_request_id' => $operation->id,
+                    ]);
+
                     Notification::send($sellers,new NewRequestNotification($data));
                     $this->pushNotification('Vous avez une nouvelle demande','nouvelle demande',$seller_ids,'sellers');
                 }
 
         }
         event(new NewRequestEvent($data));
-        $temp = [];
-        foreach ($distances as $distance)
-        {
-            $temp[] = $distance['distance'];
-        }
-        sort($temp);
-
-        foreach ($distances as $distance)
-        {
-            if(in_array($distance['distance'],$temp))
-            {
-                $seller = Seller::find($distance['seller_id']);
-                $seller->requests()->create([
-                    'user_request_id' => $operation->id,
-                ]);
-            }
-        }
         return response(['success' => true,'request_id' => $operation->id],201);
     }
 
